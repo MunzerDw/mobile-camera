e7accb310b2ccc6d37b074bf3ed9724b5793f8dc
Munzer Dwedari <munzerdwedar@gmail.com>
2020-06-09 13:29:23 +0200

    remove warnings

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,11 @@
-import 'dart:async';
-import 'dart:math';
 import 'package:App/Models/CameraModel.dart';
 import 'package:App/Models/ImageDisplayModel.dart';
 import 'package:App/Pages/Galleries/Galleries.dart';
 import 'package:flutter/material.dart';
-import 'package:camera/camera.dart';
 import 'Pages/Camera/Camera.dart';
 import 'Pages/Settings/Settings.dart';
-import "Storage.dart";
-import "package:shared_preferences/shared_preferences.dart";
 import 'package:provider/provider.dart';
 import './Models/GalleriesModel.dart';
-import './Models/GalleryModel.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
